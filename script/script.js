0599924cac71f909b08a496588b25e11ad94bce5
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-03-10 16:37:09 -0300

    menu
    feito o menu para telas pequenas com sucesso

--- script/script.js
+++ script/script.js
@@ -1,11 +1,29 @@
+function click() {
+    const material = document.querySelector('.material-icons')
+    document.addEventListener('click', (e) => {
+        const el = e.target
 
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    } else{
-        itens.style.display = 'block'
-    }
+        if (el.classList.contains('material-icons')) {
+            if (itens.style.display == 'block') {
+                itens.style.display = 'none'
+                material.innerHTML =  'menu'
+                material.style.color = 'black'
+                material.style.background = '#ffae42'
+
+            } else {
+                itens.style.display = 'block'
+                material.style.color = '#ffae42'
+                material.style.background = 'black'
+                material.style.border ='1px solid #ffae42'
+                material.innerHTML =  'close'
+                
+            }
+        } 
+
+        if(el.classList.contains('shop')) {
+            alert('Página em desenvolvimento')
+        }
+    })
+
 }
-function clickShop(){
-    alert('Página em desenvolvimento')
-}
+click()
