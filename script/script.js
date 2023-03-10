4d8e445f3b7f8082367a5b53a783cb6606cfc216
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-03-10 00:34:23 -0300

    atualização icone hamburguer
    feito a interação do icone hamburguer

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+function clickShop(){
+    alert('Página em desenvolvimento')
+}
