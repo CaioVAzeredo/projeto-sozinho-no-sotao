e382cd224c8a88ec3926c00079fc6ccbf58740b0
Dan-Dias <dsndias123@gmail.comgit config --global user.name Dan-Diasgit config --global user.email dsndias123@gmail.comgit config --global user.email dsndias123@gmail.com>
2023-03-19 00:23:26 -0300

    html da page vlog

--- script/script.js
+++ script/script.js
@@ -1,11 +1,40 @@
+const material = document.querySelector('.material-icons')
+function click() {
+    document.addEventListener('click', (e) => {
+        const el = e.target
 
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    } else{
-        itens.style.display = 'block'
-    }
+        if (el.classList.contains('material-icons')) {
+            if (itens.style.display == 'block') {
+                itens.style.display = 'none'
+                material.innerHTML = 'menu'
+                material.style.color = 'black'
+                material.style.background = '#ffae42'
+
+            } else {
+                itens.style.display = 'block'
+                material.style.color = '#ffae42'
+                material.style.background = 'black'
+                material.style.border = '1px solid #ffae42'
+                material.innerHTML = 'close'
+
+            }
+        }
+
+        if (el.classList.contains('shop')) {
+            alert('Página em desenvolvimento')
+        }
+    })
 }
-function clickShop(){
-    alert('Página em desenvolvimento')
+
+function tela() {
+    window.addEventListener('resize', (e) => {
+        if (window.innerWidth >= 768) {
+            itens.style.display = 'block'
+        } else {
+            itens.style.display = 'none'
+        }
+    })
 }
+tela()
+click()
+
